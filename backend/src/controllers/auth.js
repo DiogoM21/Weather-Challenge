032226cc8074207f4d60434fe3dc224463739f58
Diogo Mendes <diiogomendes21@hotmail.com>
2024-03-04 22:47:17 +0000

    Packages Update

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const util = require('util');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
