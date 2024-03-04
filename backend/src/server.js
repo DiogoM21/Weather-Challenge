dcd70ae0d808ea5ea06e98abdde505ce261ccd71
Diogo Mendes <diiogomendes21@hotmail.com>
2024-03-04 03:33:28 +0000

    Connection to DB Retry in Back-End

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,33 +14,41 @@ const db = mysql.createConnection({
     database: 'glartek-challenge',
 });
 
-// Connect to MySQL
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-    } else {
-        console.log('Connected to database.');
+// Function to connect to database
+async function connectWithRetry(db) {
+    while (db.state !== 'disconnected') {
+        try {
+            await db.connect();
+            console.log('Connected to database.');
+            break;
+        } catch (err) {
+            console.error('Error connecting to database. Retrying in 5 seconds.');
+            await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
     }
-});
+}
 
-// Make db accessible to our router
-app.use((req, res, next) => {
-    req.db = db;
-    next();
-});
+// Connect to database
+connectWithRetry(db).then(() => {
+    // Make db accessible to our router
+    app.use((req, res, next) => {
+        req.db = db;
+        next();
+    });
 
-// Middleware
-app.use(cors());
+    // Middleware
+    app.use(cors());
 
-// Controllers
-const homeController = require('./controllers/home');
-const cityController = require('./controllers/city');
+    // Controllers
+    const homeController = require('./controllers/home');
+    const cityController = require('./controllers/city');
 
-// Routes
-app.use('/', homeController);
-app.use('/cities', cityController);
+    // Routes
+    app.use('/', homeController);
+    app.use('/cities', cityController);
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+    // Start the server
+    app.listen(port, () => {
+        console.log(`Server is listening at http://localhost:${port}`);
+    });
 });
